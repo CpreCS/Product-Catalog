b84a0494604103ff2c9a191ec29fb455fcc1f38b
Cameron Preston <cpreston.cs@gmail.com>
2025-10-26 01:17:52 -0500

    Scaling and layout updates

--- scripts.js
+++ scripts.js
@@ -11,35 +11,30 @@ function cartCount(){
   console.log(cart.length)
 } 
 
+/* Hides Catalog and Shows Cart Content*/
 function hideContent(){
   const viewCart = document.querySelector('.cart-btn');
-  const filters = document.querySelector('.filters');
-  const search = document.querySelector('.search');
-  const products = document.getElementById('product-list');
-  const cartCount = document.querySelector('.cart-count');
+  const closeCart = document.querySelector('.close-cart-btn');
+  const main = document.querySelector('main');
+  const cartWrapper = document.querySelector('.cart-page-wrapper'); 
   let cartOpen = false;
 
   viewCart.addEventListener('click', ()=>{
     if(!cartOpen){
-      history.replaceState({}, '','/index.html/cart');
-      filters.classList.add('hidden');
-      search.classList.add('hidden');
-      products.classList.add('hidden');
-      cartCount.classList.add('hidden');
-      viewCart.classList.add('active');
-      viewCart.innerText = "Close Cart";
+      //history.replaceState({}, '','/index.html/cart');
+      main.classList.add('hidden');
+      cartWrapper.classList.add('active');
       cartOpen = true;
-    } else{
-      history.replaceState({}, '','/index.html');
-      filters.classList.remove('hidden');
-      search.classList.remove('hidden');
-      products.classList.remove('hidden');
-      cartCount.classList.remove('hidden');
-      viewCart.classList.remove('active');
-      viewCart.innerHTML = `<i class="bi bi-cart3"></i>Cart`;
+    }
+  });
+
+  closeCart.addEventListener('click', ()=>{
+    if(cartOpen){
+      //history.replaceState({}, '','/index.html');
+      cartWrapper.classList.remove('active');
+      main.classList.remove('hidden');
       cartOpen = false;
     }
-  
   });
 }
 
@@ -56,13 +51,16 @@ function cardGenerate(products){
               <img src="${product.thumbnail}" alt="${product.title}">
             </div>
             <div class="product-name" title="${product.title}">
-            <p>${product.title}</p>
+              <p>${product.title}</p>
             </div>
             <div class="product-cost">
-            <p>$${product.price.toFixed(2)}</p>
+              <p>$${product.price.toFixed(2)}</p>
             </div>
-              <div class="product-rating">
-            ${'★'.repeat(Math.round(product.rating))}${'☆'.repeat(5 - Math.round(product.rating))}
+            <div class="product-desc" title="${product.description}">
+              <p>${product.description}</p>
+            </div>
+            <div class="product-rating">
+              ${'★'.repeat(Math.round(product.rating))}${'☆'.repeat(5 - Math.round(product.rating))}
             </div>
             <div class="product-add-button">
               <button class="add">Add to Cart</button>
@@ -95,8 +93,13 @@ getProducts().then(data => {
         cardGenerate(filtered);
       }
 
+      filterButtons.forEach(btn => btn.classList.remove('active'));
+      button.classList.add('active');
     });
   })
+
+  filterButtons[0].classList.add('active');
+
   const searchBox = document.querySelector('input');
     searchBox.addEventListener('input', () => {
     const searchTerm = searchBox.value.toLowerCase();

d9ed528f88b3743ecd91065f08ce936568718522
Cameron Preston <cpreston.cs@gmail.com>
2025-10-27 12:48:34 -0500

    Finished Product Catalog Project

--- scripts.js
+++ scripts.js
@@ -5,13 +5,42 @@ async function getProducts(){
 }
 
 
-let cart = [];
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
+function saveCart(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function clearCart(){
+  const clearBtn = document.querySelector('.clear');
+
+  clearBtn.addEventListener('click', ()=>{
+    localStorage.removeItem('cart');
+    cart = [];
+    cartListing();
+    cartCount();
+  });
+}
+
+//Cart Item Count Calculator
 function cartCount(){
-  console.log(cart.length)
+  const counter = document.querySelector('.cart-count');
+  let numItems = 0; 
+  
+  cart.forEach(item =>{
+    numItems += item.quantity;
+  })
+
+  counter.innerHTML = `${numItems}`
+  if(numItems > 0){
+    counter.style.opacity = '1';
+  } else{
+    counter.style.opacity = '0';
+  }
+  return numItems
 } 
 
-/* Hides Catalog and Shows Cart Content*/
+//Hides Catalog and Shows Cart Content
 function hideContent(){
   const viewCart = document.querySelector('.cart-btn');
   const closeCart = document.querySelector('.close-cart-btn');
@@ -21,7 +50,6 @@ function hideContent(){
 
   viewCart.addEventListener('click', ()=>{
     if(!cartOpen){
-      //history.replaceState({}, '','/index.html/cart');
       main.classList.add('hidden');
       cartWrapper.classList.add('active');
       cartOpen = true;
@@ -30,7 +58,6 @@ function hideContent(){
 
   closeCart.addEventListener('click', ()=>{
     if(cartOpen){
-      //history.replaceState({}, '','/index.html');
       cartWrapper.classList.remove('active');
       main.classList.remove('hidden');
       cartOpen = false;
@@ -66,21 +93,26 @@ function cardGenerate(products){
               <button class="add">Add to Cart</button>
             </div>
           </div>
-        `
+        `;
+
         const button = card.querySelector('.add');
         button.addEventListener('click', () => {
-          cart.push(product);
+          if (button.disabled) return;
+          quantCount(product);
           console.log(`${product.title} was added to the cart.`);
           console.log(cart)
+          cartListing();
           cartCount()
 
           //"Added" effect on add to cart click
-          const originalText = button.innerText;
+          
           button.innerText = "✓ Added";
+          button.disabled = true;
           button.style.backgroundColor = "#5cc85c";
 
           setTimeout(() => {
-            button.innerText = originalText;
+            button.innerText = 'Add to Cart';
+            button.disabled = false;
             button.style.backgroundColor = "#736cfe";
           }, 600);
         })
@@ -123,4 +155,91 @@ getProducts().then(data => {
   })
 })
 
+//Checkout Cart Quantity Tracker for +/- Buttons & Total
+function quantCount(product){
+  let productDupe = cart.find(dupe => dupe.title === product.title);
+  
+    if(productDupe){
+      productDupe.quantity++
+    } else{
+      cart.push({...product, quantity: 1});
+    }
+  };
+
+
+//Cart Item Listing Card Creation
+function cartListing(){
+  const checkout = document.querySelector('.t-cost');
+  const getItemWrap = document.querySelector('.item-wrapper');
+  getItemWrap.innerHTML = "";
+
+  if (cart.length === 0){
+    getItemWrap.innerHTML = `<p class="empty-cart">Your cart is empty.</p>`;
+    checkout.innerText = "$0.00"
+    return;
+  }
+  
+  let totalCheckout = 0;
+
+  cart.forEach((product, index) => {
+    const itemPrice = (product.quantity * product.price);
+    
+    totalCheckout += itemPrice;
+
+      const listing = document.createElement('div');
+      listing.classList.add('item-list');
+      listing.innerHTML = `
+          <div class="checkout-img">
+            <img src="${product.thumbnail}" alt="${product.title}">
+          </div>
+          <div class="checkout-name-cost">
+            <p class="checkout-name" title="${product.title}">${product.title}</p>
+            <p class="checkout-cost">${product.price.toFixed(2)}</p>
+          </div>
+          <div class="checkout-item-count">
+            <button class="decrease">-</button>
+            <p class="item-count">${product.quantity}</p>
+            <button class="increase">+</button>
+          </div>
+          <div class="checkout-item-total">
+            <p class="item-count-price">$${itemPrice.toFixed(2)}</p>
+            <p class="remove">Remove</p>
+          </div>
+          `;
+
+      getItemWrap.appendChild(listing);
+      saveCart()
+      
+      const incr = listing.querySelector('.increase');
+      const decr = listing.querySelector('.decrease');
+      const remove = listing.querySelector('.remove');
+    
+      incr.addEventListener('click', ()=>{
+        cart[index].quantity++;
+        cartListing();
+        cartCount();
+      });
+      
+      decr.addEventListener('click', ()=>{
+        if(cart[index].quantity > 1){
+          cart[index].quantity--;
+          cartListing();
+          cartCount();
+        }
+      });
+      
+      remove.addEventListener('click', ()=>{
+        cart.splice(index, 1);
+        saveCart()
+        cartListing();
+        cartCount();
+    });
+  });
+  
+  checkout.innerHTML = `Total $${totalCheckout.toFixed(2)}`;
+}
+
+clearCart();
+cartCount();
+cartListing();
 hideContent();
